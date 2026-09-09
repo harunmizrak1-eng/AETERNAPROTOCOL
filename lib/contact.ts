807373338d72f981ebe86686c4de4bb1c2544187
Harun <harunmizrak1@gmail.com>
2026-09-09 08:38:42 +0300

    Update WhatsApp contact number

--- lib/contact.ts
+++ lib/contact.ts
@@ -4,7 +4,7 @@
 //
 /** Kullanıcının verdiği Türkiye numarası. wa.me için başına 90 eklenir;
  * baştaki 0 ve + işareti WhatsApp bağlantısında kullanılmaz. */
-export const whatsappLocalNumber = "5365799647"
+export const whatsappLocalNumber = "5822050187"
 export const whatsappNumber = `90${whatsappLocalNumber}`
 export const instagramHandle = "@zphc.tr"
 export const instagramUrl = "https://www.instagram.com/zphc.tr/"
